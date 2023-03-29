b318a3b965fc069779679b62dd16caece9b6c6f7
juansezanelli <juan.zanelli@ing.austral.edu.ar>
2023-03-29 12:17:49 -0300

    Implemented Logout

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,36 @@
 import {Link} from "react-router-dom";
+import {useStudability} from "../service/Studability";
+import {useState} from "react";
+import {useNavigate} from "react-router";
+import {useAuthProvider} from "../auth/auth"
+
 
 export default function HomePage() {
+    const auth = useAuthProvider()
+    const studability = useStudability()
+    const [errorMsg, setErrorMsg] = useState(undefined)
+    const navigate = useNavigate()
+
+    function logout() {
+        let userToken;
+        userToken = auth.getToken();
+        studability.logout(userToken,
+            () => {
+                auth.removeToken(userToken)
+                navigate("/", {replace: true});
+            },
+            (msg) => {
+                setErrorMsg(msg)
+            })
+    }
+
+
     return (
         <div style={{justifyContent: "center"}}>
+
             <h1>Studability, study smart</h1>
-            <Link to='./'>
-                <button type="button" className="btn btn-outline-danger">Log Out</button>
-            </Link>
+
+            <button type="button" onClick={logout} className="btn btn-outline-danger">Log Out</button>
 </div>
 
 )
