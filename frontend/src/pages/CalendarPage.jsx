07bd46958e6e16067b8dfed150b8e1ce5735c2b8
franciscocavallaro <francisco.cavallaro@ing.austral.edu.ar>
2023-03-30 15:36:57 -0300

    Menu working, calendar partially working

--- frontend/src/pages/CalendarPage.jsx
+++ frontend/src/pages/CalendarPage.jsx
@@ -1,3 +1,19 @@
-export default function CalendarPage(){
+import React, {useState} from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import '../calendarDesign.css';
 
+export default function CalendarPage() {
+
+    const [value, onChange] = useState(new Date());
+    return (
+        <div className="Calendar">
+            <header><h1>My calendar</h1></header>
+            <div className="Calendar container">
+                <main className="Calendar container content">
+                    <Calendar onChange={onChange} value={value}/>
+                </main>
+            </div>
+        </div>
+    );
 }
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,6 +5,8 @@ import {useNavigate} from "react-router";
 import {useAuthProvider} from "../auth/auth"
 import button from "bootstrap/js/src/button";
 import Dropdown from 'react-bootstrap/Dropdown';
+import StudabilityLogo from "../images/StudabilityLogo.png";
+import * as React from "react";
 
 
 export default function HomePage() {
@@ -29,23 +31,50 @@ export default function HomePage() {
 
     return (
         <div>
-            <div style={{justifyContent: "center", alignItems: "center", display: "flex"}}>
-                <h1>Studability, study smart</h1>
-            </div>
-
+            <br/>
             <div>
-                <Dropdown>
-                    <Dropdown.Toggle variant="success" id="dropdown-basic">
-                        Menu
-                    </Dropdown.Toggle>
+                <div>
 
-                    <Dropdown.Menu>
-                        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown>
+                    <div style={{justifyContent: "center", alignItems: "center", display: "flex"}}>
+                        <img style={{width: 90, height: 90}} src={StudabilityLogo} className="logo" alt=""/>
+                        <h1>Studability, study smart</h1>
+
+                        <div style={{marginLeft: 650}}>
+                            <Dropdown>
+
+                                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                                    Menu
+                                </Dropdown.Toggle>
+
+                                <Dropdown.Menu>
+                                    <Dropdown.Item href="/home/calendar">Calendar</Dropdown.Item>
+                                    <Dropdown.Item href="/files">Files</Dropdown.Item>
+                                    <Dropdown.Item href="/friends">Friends</Dropdown.Item>
+                                    <Dropdown.Item href="/studytime">StudyTime</Dropdown.Item>
+                                    <Dropdown.Item href="/profile">Profile</Dropdown.Item>
+                                </Dropdown.Menu>
+
+                            </Dropdown>
+                        </div>
+                    </div>
+                </div>
             </div>
+
+            {/*<div style={{justifyContent: "flex-end", alignItems: "center", display: "flex"}}>*/}
+            {/*    <Dropdown>*/}
+            {/*        <Dropdown.Toggle variant="success" id="dropdown-basic">*/}
+            {/*            Menu*/}
+            {/*        </Dropdown.Toggle>*/}
+
+            {/*        <Dropdown.Menu>*/}
+            {/*            <Dropdown.Item href="#/calendar">Calendar</Dropdown.Item>*/}
+            {/*            <Dropdown.Item href="#/action-2">Files</Dropdown.Item>*/}
+            {/*            <Dropdown.Item href="#/action-3">Friends</Dropdown.Item>*/}
+            {/*            <Dropdown.Item href="#/action-2">StudyTime</Dropdown.Item>*/}
+            {/*            <Dropdown.Item href="#/action-2">Profile</Dropdown.Item>*/}
+            {/*        </Dropdown.Menu>*/}
+            {/*    </Dropdown>*/}
+            {/*</div>*/}
 
             <div style={{display: "flex", justifyContent: "center"}}>
                 <button type="button" onClick={logout} className="btn btn-outline-danger">Log Out</button>
